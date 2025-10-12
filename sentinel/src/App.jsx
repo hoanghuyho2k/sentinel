961435cd6d1bb8a95d39311f4ca8c91e1a865b09
Son Dinh Nguyen Nhat <dinhnguyennhatson@icloud.com>
2025-10-12 22:33:14 +1100

    App setup

---
+++ sentinel/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+let app_name = "Sentinel";
+
+function App() {
+    return (
+        <div>
+            <h1>{app_name.toUpperCase()} Front-End!</h1>
+        </div>
+    )
+}
+
+export default App
